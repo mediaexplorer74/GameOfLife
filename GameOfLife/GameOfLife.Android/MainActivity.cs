1017432a798a6c40c7b4363c494fc8c57b2ca4ac
Media Explorer <mediaexplorer74@hotmail.com>
2020-12-21 20:08:58 +0300

    Build 1.0.1.0

---
+++ GameOfLife/GameOfLife.Android/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+
+namespace GameOfLife.Droid
+{
+    [Activity(Label = "GameOfLife", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            Xamarin.Essentials.Platform.Init(this, bundle);
+            LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
+
